79cf2470adabd74d769e456353e4917927edc090
jotase <jota.segovia@gmail.com>
2017-11-22 00:34:41 -0300

    * Refactor message * add validation for calculate an empty stack

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -2,7 +2,8 @@ require 'rpn_calculator/version'
 
 module RpnCalculator
   OPERATORS = %w[+ - * /].freeze
-  ERROR_MESSAGE = "Please enter only numbers or this operators : #{OPERATORS}".freeze
+  INVALID_ERROR = "Please enter only numbers or this operators : + - * /".freeze
+  EMPTY_STACK_ERROR = 'There are no numbers to calculate'.freeze
 
   # Class that will handle and operate the calculation functions
   class Calculator
@@ -13,15 +14,12 @@ module RpnCalculator
 
     # public method that will handle the calculator's input
     def perform(input)
-      return ERROR_MESSAGE unless valid?(input)
+      return INVALID_ERROR unless valid?(input)
       # update @tokens with the new input
       @tokens.push input
 
       # evaluate and take action by the input
       evaluate
-
-      # Return the last value or the result if the operation is completed
-      @stack.last
     end
 
     private
@@ -37,9 +35,12 @@ module RpnCalculator
         if numeric?(token)
           @stack.push(token.to_f)
         else # if is a operator calculate
+          return EMPTY_STACK_ERROR if @stack.empty?
           calculate(token)
         end
       end
+      # Return the last value or the result if the operation is completed
+      @stack.last
     end
 
     # Calculate the last 2 numbers in the stack
